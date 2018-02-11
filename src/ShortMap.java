7fe2797c9f2986e5042a134bf566d88f85571a9d
fidgetsinner <gpimpale29@gmail.com>
2018-02-11 13:23:38 -0800

    something

--- src/ShortMap.java
+++ src/ShortMap.java
@@ -29,21 +29,21 @@ public class ShortMap {
 		xSize = map.length;
 		ySize = map[0].length;
 	}
-	
+
 	public ShortMap(int xsize, int ysize)
 	{
 		xSize = xsize;
 		ySize = ysize;
 		map = new short[xSize][ySize];
 	}
-	
+
 	public ShortMap(BufferedImage img)
 	{
 		map = getValues(img);
 		xSize = map.length;
 		ySize = map[0].length;
 	}
-	
+
 	public ShortMap(String filelocation)
 	{
 		BufferedImage img = null;
@@ -58,7 +58,7 @@ public class ShortMap {
 		xSize = map.length;
 		ySize = map[0].length;
 	}
-	
+
 	public short[][] getMap()
 	{
 		return map;
@@ -89,32 +89,47 @@ public class ShortMap {
 	{	
 		return new ShortMap(ProcessImage(getImage(), radius));
 	}
-	
+
 	public static BufferedImage ProcessImage(BufferedImage image, int radius) {
-	    int width = image.getWidth();
-	    int height = image.getHeight();
+		int width = image.getWidth();
+		int height = image.getHeight();
 
-	    int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
-	    int[] changedPixels = new int[pixels.length];
+		int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
+		int[] changedPixels = new int[pixels.length];
 
-	    FastGaussianBlur(pixels, changedPixels, width, height, radius);
+		FastGaussianBlur(pixels, changedPixels, width, height, radius);
 
-	    BufferedImage newImage = new BufferedImage(width, height, image.getType());
-	    newImage.setRGB(0, 0, width, height, changedPixels, 0, width);
+		BufferedImage newImage = new BufferedImage(width, height, image.getType());
+		newImage.setRGB(0, 0, width, height, changedPixels, 0, width);
 
-	    return newImage;
+		return newImage;
 	}
-	
+
+	public ShortMap scale(double multiplier)
+	{
+		short[][] newMap = new short[xSize][ySize]; 
+		for(int x = 0; x < xSize; x++)
+		{
+			for(int y = 0; y < ySize; y++)
+			{
+				double newVal = ShortMap.ShortToDouble(map[x][y]) * multiplier;
+				newMap[x][y] =  ShortMap.DoubleToShort(clamp(newVal, 0, 1));
+			}
+		}
+		return new ShortMap(newMap);
+	}
+
+
 	public ShortMap scale(int newxsize, int newysize)
 	{
 		return new ShortMap(getValues(scale(getImage(), newxsize, newysize)));
 	}
 
 	public ShortMap truncate(int x, int y, int width, int height)
-	{	
+	{
 		return new ShortMap(getImage().getSubimage(x, y, width, height));
 	}
-	
+
 	public void Export(String filelocation)
 	{
 		try
@@ -127,117 +142,117 @@ public class ShortMap {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void FastGaussianBlur(int[] source, int[] output, int width, int height, int radius) {
-	    ArrayList<Integer> gaussianBoxes = CreateGausianBoxes(radius, 3);
-	    BoxBlur(source, output, width, height, (gaussianBoxes.get(0) - 1) / 2);
-	    BoxBlur(output, source, width, height, (gaussianBoxes.get(1) - 1) / 2);
-	    BoxBlur(source, output, width, height, (gaussianBoxes.get(2) - 1) / 2);
+		ArrayList<Integer> gaussianBoxes = CreateGausianBoxes(radius, 3);
+		BoxBlur(source, output, width, height, (gaussianBoxes.get(0) - 1) / 2);
+		BoxBlur(output, source, width, height, (gaussianBoxes.get(1) - 1) / 2);
+		BoxBlur(source, output, width, height, (gaussianBoxes.get(2) - 1) / 2);
 	}
 
 	public static ArrayList<Integer> CreateGausianBoxes(double sigma, int n) {
-	    double idealFilterWidth = Math.sqrt((12 * sigma * sigma / n) + 1);
+		double idealFilterWidth = Math.sqrt((12 * sigma * sigma / n) + 1);
 
-	    int filterWidth = (int) Math.floor(idealFilterWidth);
+		int filterWidth = (int) Math.floor(idealFilterWidth);
 
-	    if (filterWidth % 2 == 0) {
-	        filterWidth--;
-	    }
+		if (filterWidth % 2 == 0) {
+			filterWidth--;
+		}
 
-	    int filterWidthU = filterWidth + 2;
+		int filterWidthU = filterWidth + 2;
 
-	    double mIdeal = (12 * sigma * sigma - n * filterWidth * filterWidth - 4 * n * filterWidth - 3 * n) / (-4 * filterWidth - 4);
-	    double m = Math.round(mIdeal);
+		double mIdeal = (12 * sigma * sigma - n * filterWidth * filterWidth - 4 * n * filterWidth - 3 * n) / (-4 * filterWidth - 4);
+		double m = Math.round(mIdeal);
 
-	    ArrayList<Integer> result = new ArrayList<>();
+		ArrayList<Integer> result = new ArrayList<>();
 
-	    for (int i = 0; i < n; i++) {
-	        result.add(i < m ? filterWidth : filterWidthU);
-	    }
+		for (int i = 0; i < n; i++) {
+			result.add(i < m ? filterWidth : filterWidthU);
+		}
 
-	    return result;
+		return result;
 	}
 
 	private static void BoxBlur(int[] source, int[] output, int width, int height, int radius) {
-	    System.arraycopy(source, 0, output, 0, source.length);
-	    BoxBlurHorizontal(output, source, width, height, radius);
-	    BoxBlurVertical(source, output, width, height, radius);
+		System.arraycopy(source, 0, output, 0, source.length);
+		BoxBlurHorizontal(output, source, width, height, radius);
+		BoxBlurVertical(source, output, width, height, radius);
 	}
 
 	private static void BoxBlurHorizontal(int[] sourcePixels, int[] outputPixels, int width, int height, int radius) {
-	    int resultingColorPixel;
-	    float iarr = 1f / (radius + radius);
-	    for (int i = 0; i < height; i++) {
-	        int outputIndex = i * width;
-	        int li = outputIndex;
-	        int sourceIndex = outputIndex + radius;
+		int resultingColorPixel;
+		float iarr = 1f / (radius + radius);
+		for (int i = 0; i < height; i++) {
+			int outputIndex = i * width;
+			int li = outputIndex;
+			int sourceIndex = outputIndex + radius;
 
-	        int fv = Byte.toUnsignedInt((byte) sourcePixels[outputIndex]);
-	        int lv = Byte.toUnsignedInt((byte) sourcePixels[outputIndex + width - 1]);
-	        float val = (radius) * fv;
+			int fv = Byte.toUnsignedInt((byte) sourcePixels[outputIndex]);
+			int lv = Byte.toUnsignedInt((byte) sourcePixels[outputIndex + width - 1]);
+			float val = (radius) * fv;
 
-	        for (int j = 0; j < radius; j++) {
-	            val += Byte.toUnsignedInt((byte) (sourcePixels[outputIndex + j]));
-	        }
+			for (int j = 0; j < radius; j++) {
+				val += Byte.toUnsignedInt((byte) (sourcePixels[outputIndex + j]));
+			}
 
-	        for (int j = 0; j < radius; j++) {
-	            val += Byte.toUnsignedInt((byte) sourcePixels[sourceIndex++]) - fv;
-	            resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
-	            outputPixels[outputIndex++] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
-	        }
+			for (int j = 0; j < radius; j++) {
+				val += Byte.toUnsignedInt((byte) sourcePixels[sourceIndex++]) - fv;
+				resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
+				outputPixels[outputIndex++] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
+			}
 
-	        for (int j = (radius + 1); j < (width - radius); j++) {
-	            val += Byte.toUnsignedInt((byte) sourcePixels[sourceIndex++]) - Byte.toUnsignedInt((byte) sourcePixels[li++]);
-	            resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
-	            outputPixels[outputIndex++] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
-	        }
+			for (int j = (radius + 1); j < (width - radius); j++) {
+				val += Byte.toUnsignedInt((byte) sourcePixels[sourceIndex++]) - Byte.toUnsignedInt((byte) sourcePixels[li++]);
+				resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
+				outputPixels[outputIndex++] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
+			}
 
-	        for (int j = (width - radius); j < width; j++) {
-	            val += lv - Byte.toUnsignedInt((byte) sourcePixels[li++]);
-	            resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
-	            outputPixels[outputIndex++] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
-	        }
-	    }
+			for (int j = (width - radius); j < width; j++) {
+				val += lv - Byte.toUnsignedInt((byte) sourcePixels[li++]);
+				resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
+				outputPixels[outputIndex++] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
+			}
+		}
 	}
 
 	private static void BoxBlurVertical(int[] sourcePixels, int[] outputPixels, int width, int height, int radius) {
-	    int resultingColorPixel;
-	    float iarr = 1f / (radius + radius + 1);
-	    for (int i = 0; i < width; i++) {
-	        int outputIndex = i;
-	        int li = outputIndex;
-	        int sourceIndex = outputIndex + radius * width;
+		int resultingColorPixel;
+		float iarr = 1f / (radius + radius + 1);
+		for (int i = 0; i < width; i++) {
+			int outputIndex = i;
+			int li = outputIndex;
+			int sourceIndex = outputIndex + radius * width;
 
-	        int fv = Byte.toUnsignedInt((byte) sourcePixels[outputIndex]);
-	        int lv = Byte.toUnsignedInt((byte) sourcePixels[outputIndex + width * (height - 1)]);
-	        float val = (radius + 1) * fv;
+			int fv = Byte.toUnsignedInt((byte) sourcePixels[outputIndex]);
+			int lv = Byte.toUnsignedInt((byte) sourcePixels[outputIndex + width * (height - 1)]);
+			float val = (radius + 1) * fv;
 
-	        for (int j = 0; j < radius; j++) {
-	            val += Byte.toUnsignedInt((byte) sourcePixels[outputIndex + j * width]);
-	        }
-	        for (int j = 0; j <= radius; j++) {
-	            val += Byte.toUnsignedInt((byte) sourcePixels[sourceIndex]) - fv;
-	            resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
-	            outputPixels[outputIndex] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
-	            sourceIndex += width;
-	            outputIndex += width;
-	        }
-	        for (int j = radius + 1; j < (height - radius); j++) {
-	            val += Byte.toUnsignedInt((byte) sourcePixels[sourceIndex]) - Byte.toUnsignedInt((byte) sourcePixels[li]);
-	            resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
-	            outputPixels[outputIndex] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
-	            li += width;
-	            sourceIndex += width;
-	            outputIndex += width;
-	        }
-	        for (int j = (height - radius); j < height; j++) {
-	            val += lv - Byte.toUnsignedInt((byte) sourcePixels[li]);
-	            resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
-	            outputPixels[outputIndex] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
-	            li += width;
-	            outputIndex += width;
-	        }
-	    }
+			for (int j = 0; j < radius; j++) {
+				val += Byte.toUnsignedInt((byte) sourcePixels[outputIndex + j * width]);
+			}
+			for (int j = 0; j <= radius; j++) {
+				val += Byte.toUnsignedInt((byte) sourcePixels[sourceIndex]) - fv;
+				resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
+				outputPixels[outputIndex] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
+				sourceIndex += width;
+				outputIndex += width;
+			}
+			for (int j = radius + 1; j < (height - radius); j++) {
+				val += Byte.toUnsignedInt((byte) sourcePixels[sourceIndex]) - Byte.toUnsignedInt((byte) sourcePixels[li]);
+				resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
+				outputPixels[outputIndex] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
+				li += width;
+				sourceIndex += width;
+				outputIndex += width;
+			}
+			for (int j = (height - radius); j < height; j++) {
+				val += lv - Byte.toUnsignedInt((byte) sourcePixels[li]);
+				resultingColorPixel = Byte.toUnsignedInt(((Integer) Math.round(val * iarr)).byteValue());
+				outputPixels[outputIndex] = (0xFF << 24) | (resultingColorPixel << 16) | (resultingColorPixel << 8) | (resultingColorPixel);
+				li += width;
+				outputIndex += width;
+			}
+		}
 	}
 
 	//65536 is the numbers in a short
@@ -291,12 +306,12 @@ public class ShortMap {
 		}
 		return img;
 	}
-	
+
 	public BufferedImage getImage()
 	{
 		return getImage(map);
 	}
-	
+
 	public static short[][] getValues(BufferedImage img)
 	{
 		int ySize = img.getWidth();
@@ -313,5 +328,20 @@ public class ShortMap {
 		}
 		return map;
 	}
-	
+
+	public static double clamp(double val, double min, double max)
+	{
+		return Math.max(min, Math.min(max, val));
+	}
+	/**
+	 * 
+	 * @param maps the maps to average (must be same size)
+	 * @param weights the weights of each map (Should all add up to 1, with each individual weight being in between 1 and 0)
+	 * @return The average of the maps
+	 */
+	public static ShortMap weightedAverage(ShortMap[] maps, double[] weights)
+	{
+		double[][] mapSum = new double[]
+	}
+
 }
