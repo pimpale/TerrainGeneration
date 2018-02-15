a7d4c83262bb5564190a20994514afe7fc1ea98e
fidgetsinner <gpimpale29@gmail.com>
2018-02-14 20:45:24 -0800

    made the window compile for the first time

--- src/WorldLauncher.java
+++ src/WorldLauncher.java
@@ -1,7 +1,6 @@
 import java.awt.Dimension;
 import java.awt.Font;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -9,101 +8,45 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
 
-public class WorldLauncher extends JPanel {
+public class WorldLauncher {
+	
+	
+	
+	public WorldLauncher()
+	{
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		String[] s = new String[] {"hola", "hi", "ok"};
+		
+		JList<String> list = new JList<String>(s);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setSelectedIndex(0);
+		
+		JScrollPane listScrollPane = new JScrollPane(list);
+		JLabel picture = new JLabel();
+		picture.setFont(picture.getFont().deriveFont(Font.ITALIC));
+		picture.setHorizontalAlignment(JLabel.CENTER);
 
-	 private JLabel picture;
-	    private JList list;
-	    private JSplitPane splitPane;
-	    private String[] imageNames = { "Bird", "Cat", "Dog", "Rabbit", "Pig", "dukeWaveRed",
-	        "kathyCosmo", "lainesTongue", "left", "middle", "right", "stickerface"};
-	    public WorldLauncher() {
-	 
-	        //Create the list of images and put it in a scroll pane.
-	         
-	        list = new JList(imageNames);
-	        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-	        list.setSelectedIndex(0);
-	        list.addListSelectionListener(this);
-	         
-	        
-	        JScrollPane listScrollPane = new JScrollPane(list);
-	        picture = new JLabel();
-	        picture.setFont(picture.getFont().deriveFont(Font.ITALIC));
-	        picture.setHorizontalAlignment(JLabel.CENTER);
-	         
-	        JScrollPane pictureScrollPane = new JScrollPane(picture);
-	 
-	        //Create a split pane with the two scroll panes in it.
-	        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-	                                   listScrollPane, pictureScrollPane);
-	        splitPane.setOneTouchExpandable(true);
-	        splitPane.setDividerLocation(150);
-	 
-	        //Provide minimum sizes for the two components in the split pane.
-	        Dimension minimumSize = new Dimension(100, 50);
-	        listScrollPane.setMinimumSize(minimumSize);
-	        pictureScrollPane.setMinimumSize(minimumSize);
-	 
-	        //Provide a preferred size for the split pane.
-	        splitPane.setPreferredSize(new Dimension(400, 200));
-	        updateLabel(imageNames[list.getSelectedIndex()]);
-	    }
-	     
-	    //Listens to the list
-	    public void valueChanged(ListSelectionEvent e) {
-	        JList list = (JList)e.getSource();
-	        updateLabel(imageNames[list.getSelectedIndex()]);
-	    }
-	     
-	    //Renders the selected image
-	    protected void updateLabel (String name) {
-	        ImageIcon icon = createImageIcon("images/" + name + ".gif");
-	        picture.setIcon(icon);
-	        if  (icon != null) {
-	            picture.setText(null);
-	        } else {
-	            picture.setText("Image not found");
-	        }
-	    }
-	 
-	    //Used by SplitPaneDemo2
-	    public JList getImageList() {
-	        return list;
-	    }
-	 
-	    public JSplitPane getSplitPane() {
-	        return splitPane;
-	    }
-	 
-	    
-	    /** Returns an ImageIcon, or null if the path was invalid. */
-	    protected static ImageIcon createImageIcon(String path) {
-	       java.net.URL imgURL = SplitPaneDemo.class.getResource(path);
-	        if (imgURL != null) {
-	            return new ImageIcon(imgURL);
-	        } else {
-	            System.err.println("Couldn't find file: " + path);
-	            return null;
-	        }
-	    }
-	 
-	    /**
-	     * Create the GUI and show it.  For thread safety,
-	     * this method should be invoked from the
-	     * event-dispatching thread.
-	     */
-	    private static void createAndShowGUI() {
-	 
-	        //Create and set up the window.
-	        JFrame frame = new JFrame("SplitPaneDemo");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.getContentPane().add(splitPaneDemo.getSplitPane());
-	 
-	        //Display the window.
-	        frame.pack();
-	        frame.setVisible(true);
-	    }
+		JScrollPane pictureScrollPane = new JScrollPane(picture);
 
+		//Create a split pane with the two scroll panes in it.
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+				listScrollPane, pictureScrollPane);
+		splitPane.setOneTouchExpandable(true);
+		splitPane.setDividerLocation(150);
+
+		//Provide minimum sizes for the two components in the split pane.
+		Dimension minimumSize = new Dimension(100, 50);
+		listScrollPane.setMinimumSize(minimumSize);
+		pictureScrollPane.setMinimumSize(minimumSize);
+
+		//Provide a preferred size for the split pane.
+		splitPane.setPreferredSize(new Dimension(400, 200));
+		
+		frame.getContentPane().add(splitPane);
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
