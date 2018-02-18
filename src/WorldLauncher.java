00c05620d9db478d6dc13415c3402855b1299847
team 2643 <gpimpale29@gmail.com>
2018-02-17 19:42:13 -0800

    changed layout of worldLauncher

--- src/WorldLauncher.java
+++ src/WorldLauncher.java
@@ -5,12 +5,15 @@ import java.awt.Font;
 import java.io.File;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -21,9 +24,11 @@ public class WorldLauncher implements ListSelectionListener{
 	private WorldTemplate[] templates;
 	private JList<String> templateList;
 	private JScrollPane listScrollPane;
+	private JButton runButton;
+	private JSplitPane leftSplitPane;
+	private JTextArea descriptionArea;
 	private JScrollPane descriptionScrollPane;
-	private JSplitPane infoSplitPane;
-	private JLabel thumbnail;
+	
 	private JSplitPane splitPane;
 	
 	private static WorldTemplate[] getWorldTemplates(File parentDirectory)
@@ -89,35 +94,20 @@ public class WorldLauncher implements ListSelectionListener{
 		templates = getWorldTemplates(parentDirectory);
 		templateList = getJList(templates);
 		listScrollPane = new JScrollPane(templateList);
+		runButton = new JButton("Run World");
+		leftSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, listScrollPane, runButton);
+		descriptionArea = new JTextArea(templates[0].getDescription());
+		descriptionArea.setEditable(false);
+		descriptionScrollPane = new JScrollPane(descriptionArea);
+		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+				leftSplitPane, descriptionScrollPane);
 		
 		
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
-
-		
-		JLabel picture = new JLabel();
-
-		Canvas c = new Canvas();
-		
-		c.setBackground(Color.BLACK);
-		
-		JScrollPane pictureScrollPane = new JScrollPane(c);
-
-		//Create a split pane with the two scroll panes in it.
-		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-				listScrollPane, pictureScrollPane);
-		splitPane.setOneTouchExpandable(true);
-		splitPane.setDividerLocation(150);
-
-		//Provide minimum sizes for the two components in the split pane.
-		listScrollPane.setMinimumSize(new Dimension(100, 50));
-		pictureScrollPane.setMinimumSize(new Dimension(100, 50));
-
-		//Provide a preferred size for the split pane.
-		splitPane.setPreferredSize(new Dimension(400, 200));
-		
+				
+	
 		frame.getContentPane().add(splitPane);
 		frame.pack();
 		frame.setVisible(true);
@@ -125,7 +115,8 @@ public class WorldLauncher implements ListSelectionListener{
 	
 	@Override
 	public void valueChanged(ListSelectionEvent arg0) {
-		arg0.getFirstIndex();
+		WorldTemplate selectedTemplate = templates[arg0.getFirstIndex()];
+		descriptionArea.setText(selectedTemplate.getDescription());
 	}
 	
 	
