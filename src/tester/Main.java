c8195bdb565bba16ee5920dbf1f920ea98ddf5d7
Govind Pimpale <gpimpale29@gmail.com>
2018-04-08 15:28:51 -0700

    fixed library issues

--- src/tester/Main.java
+++ src/tester/Main.java
@@ -1,14 +1,21 @@
 package tester;
 import java.io.File;
 
+import com.flowpowered.noise.module.source.Billow;
+
+import worldUtils.ShortMap;
+import worldUtils.WorldUtils;
+
 @SuppressWarnings("serial")
 public class Main
 {
 	
 	public static void main(String[] args) throws InterruptedException 
-	{
-	
+	{		
+		System.out.println(System.getProperty("java.class.path"));
 		
+		ShortMap sh = WorldUtils.noise(new Billow(), 300, 300);
+		sh.Export("/home/fidgetsinner/img.png");
 	}
 
 }
